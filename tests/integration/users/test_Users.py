28f19303f3806118c87c959cdbe2094e08867b67
t.onodera <t.onodera.1.2.1.5@gmail.com>
2021-12-11 19:14:49 +0900

    wrote user update api.

--- tests/integration/users/test_Users.py
+++ tests/integration/users/test_Users.py
@@ -1,3 +1,4 @@
+from _pytest.monkeypatch import resolve
 import pytest
 
 from fastapi.testclient import TestClient
@@ -40,4 +41,33 @@ def test_read_users():
     assert response.status_code == 200
     assert request_body["name"] == response_data["name"]
     assert request_body["email"] == response_data["email"]
+    assert "password" not in response_data
+
+
+@pytest.mark.skip
+def update():
+
+    # 登録リクエスト
+    request_body = {
+        "name": "testuser",
+        "email": "test@test.com",
+        "password": "very_secret_code",
+    }
+    response = client.post("/users/create", json=request_body)
+    registered_id = response.json()["id"]
+
+    # 更新リクエスト
+    update_body = {
+        "name": "update_test_user",
+        "email": "update_test@test.com",
+        "password": "update_password",
+    }
+    response = client.put(f"/users/{registered_id}")
+    assert response.status_code == 200
+
+    # 更新を確認
+    response = client.read(f"/users/{registered_id}")
+    response_data = response.json()
+    assert update_body["name"] == response_data["name"]
+    assert update_body["email"] == response_data["email"]
     assert "password" not in response_data
