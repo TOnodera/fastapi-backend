6717f7281126d078d8ef69c73a098e1b3df49417
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-01-15 15:28:47 +0900

    CORS対応

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers.routers import router
 
 
@@ -9,3 +10,13 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
+# CORS対応
+origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
