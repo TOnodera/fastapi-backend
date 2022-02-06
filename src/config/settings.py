01fa2f54ac3385b1fc5889d2cf2297e2d8859627
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-02-07 00:33:18 +0900

    ファイル取得API作成

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,4 +3,4 @@ FILES_DIR = "/home/python/app/storages"
 # ユーザーファイルのストレージ
 USER_FILES_DIR = "/home/python/app/storages/users/"
 # クライアント用ファイルディレクトリパス
-CLIENT_STORAGE_DIR = "/storage"
+CLIENT_STORAGE_DIR = "http://localhost:8000/storages"
---
+++ src/routers/storages.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from fastapi import status
+from fastapi.responses import FileResponse, JSONResponse
+from src.config import settings
+import os
+
+
+router = APIRouter()
+
+
+@router("/storage/{storage_name}/{file_name}")
+def resourses(storage_name: str, file_name: str):
+    path_file = f"{settings.FILES_DIR}/{storage_name}/{file_name}"
+    if os.path.exists(path_file):
+        return FileResponse(path_file)
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND)
